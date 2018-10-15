65282c7f530eaa85165473ecee9a52e0b55ad038
Tatyana Scotce <tscotce@gmail.com>
2018-10-15 03:24:58 +0000

    automatically backed up by learn

--- app/controllers/figures_controller.rb
+++ app/controllers/figures_controller.rb
@@ -1,3 +1,50 @@
 class FiguresController < ApplicationController
   # add controller methods
+  get '/figures' do
+    @figures = Figure.all
+    erb :"figures/index"
+  end
+  
+  get '/figures/new' do
+    @landmarks = Landmark.all
+    @titles = Title.all
+    erb :"figures/new"
+  end
+  
+  post '/figures' do
+    @figure = Figure.create(params[:figure])
+    if !params["landmark"]["name"].empty?
+      @figure.landmarks << Landmark.create(name: params["landmark"]["name"])
+    end
+    if !params["title"]["name"].empty?
+      @figure.titles << Title.create(name: params["title"]["name"])
+    end
+    redirect "figures/#{@figure.id}"
+  end
+  
+  get '/figures/:id' do
+    @figure = Figure.find(params[:id])
+    erb :"figures/show"
+  end
+  
+  get '/figures/:id/edit' do 
+    @figure = Figure.find(params[:id])
+    @landmark = @figure.landmarks
+    @title = @figure.titles
+    @landmarks = Landmark.all
+    @titles = Title.all
+    erb :"figures/edit"
+  end
+  
+  patch '/figures/:id' do 
+    # if !params[:owner].keys.include?("pet_ids")
+    # params[:owner]["pet_ids"] = []
+    # end
+    @figure.update(params["name"])
+    # if !params["pet"]["name"].empty?
+    #   @owner.pets << Pet.create(name: params["pet"]["name"])
+    # end
+    redirect "figures/#{@figure.id}"
+  end
+  
 end
--- config/environment.rb
+++ config/environment.rb
@@ -9,6 +9,7 @@ ActiveRecord::Base.establish_connection(
 )
 
 require_relative "../app/controllers/application_controller.rb"
+# require_all "./app"
 
 Dir[File.join(File.dirname(__FILE__), "../app/models", "*.rb")].each {|f| require f}
 Dir[File.join(File.dirname(__FILE__), "../app/controllers", "*.rb")].sort.each {|f| require f}
