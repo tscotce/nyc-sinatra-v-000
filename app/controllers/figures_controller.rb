ff7b52ce115b9a6f1a7fb169fb347c5a88bba304
Tatyana Scotce <tscotce@gmail.com>
2018-10-15 03:27:17 +0000

    automatically backed up by learn

--- app/controllers/figures_controller.rb
+++ app/controllers/figures_controller.rb
@@ -37,7 +37,7 @@ class FiguresController < ApplicationController
   end
   
   patch '/figures/:id' do 
-    @figure.update(name: params["figure"]["name"])
+    @figure.update(name: params["name"])
 
     redirect "figures/#{@figure.id}"
   end
